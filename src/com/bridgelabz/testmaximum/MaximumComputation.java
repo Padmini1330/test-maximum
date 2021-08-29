22af69f056410e7c5a5b2e2588e4d7b78dc8b301
padmini v <padmini.v@perfios.com>
2021-08-29 23:12:09 +0530

    ADDED: Maximum computation using 3 Integer objects

--- src/com/bridgelabz/testmaximum/MaximumComputation.java
+++ src/com/bridgelabz/testmaximum/MaximumComputation.java
@@ -6,9 +6,32 @@ public class MaximumComputation
 	public static void main(String[] args) 
 	{
 		System.out.println("***Welcome to Max Number Computation problem***");
+		Integer firstNumber=3;
+		Integer secondNumber=1;
+		Integer thirdNumber=5;
+		findMaximum(firstNumber,secondNumber,thirdNumber);
 
 	}
 
-	
+	private static void findMaximum(Integer firstNumber, Integer secondNumber, Integer thirdNumber) 
+	{
+		Integer maxNumber;
+		
+		if(firstNumber.compareTo(secondNumber)>0 && firstNumber.compareTo(thirdNumber)>0)
+		{
+			maxNumber=firstNumber;
+		}
+		else if(secondNumber.compareTo(thirdNumber)>0 && secondNumber.compareTo(firstNumber)>0)
+		{
+			maxNumber=secondNumber;
+		}
+		else
+			maxNumber=thirdNumber;
+		
+		
+		System.out.println("The maximum of Three Numbers is "+ maxNumber);
+		
+		
+	}
 
 }
