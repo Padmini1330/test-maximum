a0bbf260fe98f068088b43c2a5bfbd7d70525412
padmini v <padmini.v@perfios.com>
2021-08-29 23:07:32 +0530

    ADDED: Welcome Message

---
+++ src/com/bridgelabz/testmaximum/MaximumComputation.java
@@ -0,0 +1,14 @@
+package com.bridgelabz.testmaximum;
+
+public class MaximumComputation 
+{
+
+	public static void main(String[] args) 
+	{
+		System.out.println("***Welcome to Max Number Computation problem***");
+
+	}
+
+	
+
+}
