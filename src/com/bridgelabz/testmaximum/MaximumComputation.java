e1d560d23562d0dd5f21e42060a6f129adeb32aa
padmini v <padmini.v@perfios.com>
2021-08-30 00:06:17 +0530

    REFACTORED: created generic class to take 3 generic type variables

--- src/com/bridgelabz/testmaximum/MaximumComputation.java
+++ src/com/bridgelabz/testmaximum/MaximumComputation.java
@@ -1,7 +1,21 @@
 package com.bridgelabz.testmaximum;
 
-public class MaximumComputation 
+public class MaximumComputation  <E extends Comparable<E>> 
 {
+	E value1,value2,value3;
+	
+	public MaximumComputation(E value1,E value2,E value3) 
+	{
+		this.value1=value1;
+		this.value2=value2;
+		this.value3=value3;
+	}
+	
+	private void testMaximum() {
+		
+		MaximumComputation.findMaximum(this.value1, this.value2, this.value3);
+
+	}
 
 	public static <E extends Comparable<E>> void findMaximum(E firstNumber, E secondNumber, E thirdNumber)
 	{
@@ -29,11 +43,11 @@ public class MaximumComputation
 		Integer firstNumber=3, secondNumber=1, thirdNumber=5;
 		Double number1=1.29, number2=7.83, number3= 2.77;
 		String value1="apple" ,value2="peach", value3="banana";
-		findMaximum(firstNumber,secondNumber,thirdNumber);
-		findMaximum(number1,number2,number3);
-		findMaximum(value1,value2,value3);
+		new MaximumComputation<Integer>(firstNumber, secondNumber, thirdNumber).testMaximum();
+		new MaximumComputation<Double>(number1, number2, number3).testMaximum();
+		new MaximumComputation<String>(value1, value2, value3).testMaximum();
 
 	}
 
-
+  
 }
